e93cf77e82b37eccca5d3c2420e28fc999335d6e
Liangyi Chen <liangyi.chen@pwc.com>
2017-02-27 19:04:09 +0800

    2 phase
    record log for application

--- src/main/java/ivan/learn/springboot/controller/GreetingController.java
+++ src/main/java/ivan/learn/springboot/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package ivan.learn.springboot.controller;
 
 import org.hibernate.validator.constraints.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,18 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class GreetingController
 {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RequestMapping(method = RequestMethod.GET, value = "/hello")
 	public ResponseEntity<String> greeting(@RequestParam String name)
 	{
-		return new ResponseEntity<String>("Hello, " + name + "!",
-				HttpStatus.OK);
+		logger.info("Wellcome to springboot logback");
+		logger.debug("Wellcome to springboot logback");
+		return new ResponseEntity<String>("Hello, " + name + "!", HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/password_reset")
-	public ResponseEntity<String> forgetPassword(
-			@Email(message = "Email is not valid.") @RequestParam String email)
+	public ResponseEntity<String> forgetPassword(@Email(message = "Email is not valid.") @RequestParam String email)
 	{
-		return new ResponseEntity<String>("Reset password successfully.",
-				HttpStatus.OK);
+		return new ResponseEntity<String>("Reset password successfully.", HttpStatus.OK);
 	}
 }
